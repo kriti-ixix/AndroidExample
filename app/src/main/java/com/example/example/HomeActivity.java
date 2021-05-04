5789af20cc12abf6ca1278ef51693616d2e42162
Kriti Bhatia <kritibhatia99@gmail.com>
2021-05-04 10:28:24 +0530

    Options menu

--- app/src/main/java/com/example/example/HomeActivity.java
+++ app/src/main/java/com/example/example/HomeActivity.java
@@ -1,23 +1,47 @@
 package com.example.example;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    TextView username;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        username = findViewById(R.id.textViewLoginName);
-        Intent in = getIntent();
-        String userEmail = in.getStringExtra("email");
-        username.setText(userEmail);
+        textView = findViewById(R.id.textViewLoginName);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.menuAbout)
+        {
+            textView.setText("About is clicked");
+        }
+        else if (id == R.id.menuLogOut)
+        {
+            textView.setText("Logging out");
+        }
+
+        return true;
     }
 }
